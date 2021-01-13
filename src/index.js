0ba165599bb2a590f637ec661fe31cded07d986f
Charles Francoise <charles.francoise@gmail.com>
2021-01-13 14:41:31 +0100

    add complete test

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ export default {
   install(Vue) {
     const env = { ...process.env };
     const vueAppShortcuts = _(env)
-      .pickBy((_, k) => k.startsWith('VUE_APP'))
-      .mapKeys((_, k) => k.substring(7))
+      .pickBy((_, k) => k.startsWith('VUE_APP_'))
+      .mapKeys((_, k) => k.substring(8))
       .value();
     const camelCase = _.mapKeys({ ...env, ...vueAppShortcuts }, (_, k) => {
       if (of(k) === 'constant') {
--- tests/unit/index.spec.js
+++ tests/unit/index.spec.js
@@ -1,13 +1,33 @@
 import { createLocalVue, mount } from '@vue/test-utils';
+import { config as dotenv } from 'dotenv';
+import path from 'path';
 
 import VueEnvPlugin from '@/index';
 
 describe('Vue env plugin', () => {
+  dotenv({ path: path.join(__dirname, '.env.test') });
+
   test('loads environment into plugin', () => {
     const localVue = createLocalVue();
     localVue.use(VueEnvPlugin);
 
-    const wrapper = mount(localVue);
-    console.log(wrapper.$env);
+    // Env var
+    expect(localVue.prototype.$env).toHaveProperty('TEST_VAR', 'Hello World');
+    // Camelcase
+    expect(localVue.prototype.$env).toHaveProperty('testVar', 'Hello World');
+    // Vue env var
+    expect(localVue.prototype.$env).toHaveProperty(
+      'VUE_APP_TEST_VAR_2',
+      'Hello Vue'
+    );
+    // Vue shortcut
+    expect(localVue.prototype.$env).toHaveProperty('TEST_VAR_2', 'Hello Vue');
+    // Vue camelcase
+    expect(localVue.prototype.$env).toHaveProperty(
+      'vueAppTestVar2',
+      'Hello Vue'
+    );
+    // Vue shortcut camelcase
+    expect(localVue.prototype.$env).toHaveProperty('testVar2', 'Hello Vue');
   });
 });
